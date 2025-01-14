7ea76046c83eb3f92d95b038663491add7a8802a
aryan10092 <aryan9090900@gmail.com>
2025-01-14 23:57:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
-  build: {
+   build: {
     rollupOptions: {
       external: ['lucide-react'],
     },
   },
-};
+})
+
