956facc3441d7c6339f4920e51cf99d1799dab4a
aryan10092 <aryan9090900@gmail.com>
2025-01-14 23:36:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,9 @@ import { Avatar } from '@mui/material';
 import { FaRobot } from "react-icons/fa";
 import { Loadinglarge, Loadingsamll } from '../components/Loading';
 import { IoMdSend } from 'react-icons/io';
-
+import { Component, Send } from 'lucide-react';
+import { Bot } from 'lucide-react';
+import { Sparkles } from 'lucide-react';
 const Home = () => {
   const[isopen,setisopen]=useState(true)
   const reverse=()=>{
@@ -37,7 +39,7 @@ chats}=Chatdata()
       }
     }, [messages]);
   return (
-    <div className='text-white flex h-screen '>
+    <div className='text-white flex h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-gray-950 font-poppins overflow-hidden'>
     
     <Sidebar isopen={isopen} reverse={reverse}/>
    <div className='flex flex-1 flex-col'>
@@ -48,15 +50,15 @@ chats}=Chatdata()
   <GiHamburgerMenu />
 </button>
 {createlod?(Loadinglarge):(
-<div className="flex-1 p-6 mb-20 md:mb-0">
+<div className="flex-1 p-6 mb-20  mt-4  overflow-hidden  md:mb-0 ">
           <Header />
-          <div className='flex-1 p-6 max-h-[600px] overflow-y-auto mb-20 md:mb-0 scrollbar'
+          <div className='flex-1  rounded-3xl p-6 max-h-[600px] overflow-y-auto mb-20 md:mb-0 scrollbar '
           
           ref={messagecontainerRef}>
 {
-  messages && messages.length>0 ?(messages.map((x,i)=>(
+   messages && messages.length>0 ?(messages.map((x,i)=>(
     <div key={i}>
-      <div className='mb-4 p-4 rounded-lg bg-gray-700 text-white items-start flex gap-2'>
+      <div className='mb-4 p-4 rounded-lg bg-teal-200/40 text-teal-100 border border-gray-400/20 shadow-xl  items-start flex gap-2'>
       
         <div className={`flex-shrink-0 ${isopen ? 'hidden' : 'block'} md:block`}>
           
@@ -78,7 +80,7 @@ chats}=Chatdata()
         {x.question}
         </div>
       </div>
-      <div className='mb-4 p-4 rounded bg-[rgb(17,29,39)]  text-white flex items-start gap-2'>
+      <div className='mb-4 p-4 rounded bg-transparent bg-opacity-20   border border-gray-400/20 shadow-xl text-teal-400 flex items-start gap-2'>
         <div className='bg-white p-2 rounded-full text-black text-2xl h-10'>
           <FaRobot/>
         </div >
@@ -87,7 +89,18 @@ chats}=Chatdata()
         </div>
       </div>
 
-      </div>))):<p>Create new chat to continue</p>
+      </div>))):
+      <div className='flex flex-col items-center justify-center h-full text-gray-300/60'>
+        <Component size={48} className='mb-2 text-gray-400 mt-3'/>
+        <h2 className='text-2xl  mb-2 mt-1 text-gray-400'>Create new chat to start a conversation</h2>
+        {!isopen && (<div className='mt-2 text-slate-400 opacity-70'>
+          <p>•Avoid sharing personal or sensitive information.</p>
+          <p>•Avoid asking offensive or harmful content.</p>
+          <p>•It cannot provide any time based data.</p>
+
+        </div>)}
+      
+       </div>
   }
 
   {newrequestload && <Loadingsamll/>}
@@ -96,23 +109,24 @@ chats}=Chatdata()
           </div>)
 }
 </div>
-{chats.length===0?"":(
-<div className='fixed bottom-0 right-0 left-auto p-5 bg-gray-950 w-full md:w-[75%]'>
+{!isopen&&chats.length>0&&(
+ <div className='fixed bottom-0 right-0 left-0 md:left-[25%]  p-5  w-full md:w-[75%] z-20'> 
+
   <form  
    onSubmit={submitHandler}
   
-  className='flex justify-center items-center'>
+  >
 <input
-className='flex-grow p-4 bg-gray-700 rounded-sm text-white outline-none'
+className=' py-4 px-5 w-full bg-transparent  rounded-2xl glass-input text-blue-50 border-teal-300 border-2  focus:outline-none focus:ring-2 focus:ring-teal-400 pr-12 placeholder-teal-600 '
 type='text'
-placeholder='enter your query'
+placeholder='Enter your query'
 value={label}
 onChange={(e)=> setlabel(e.target.value)}
 required
 >
 </input>
-<button className="p-4 bg-gray-700 rounded-r text-2xl text-white">
-              <IoMdSend />
+<button className=" absolute right-2 top-1/2 -translate-y-1/2 p-8 rounded-xl  hover:text-teal-200 transition-all duration-200">
+              <Send size={20} />
             </button>
   </form>
 </div>
